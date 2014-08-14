3b8695ecad8f9fa27fbb54cb18dc66e103b1a296
Caleb Tutty <caleb@prettymint.co.nz>
2014-08-14 21:08:34 +1200

    Initial commit

--- NZ MPs/MasterViewController.swift
+++ NZ MPs/MasterViewController.swift
@@ -19,24 +19,35 @@ class MasterViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
+        requestItems()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func requestItems() {
+        Alamofire.request(.GET, "http://politicians.org.nz/parliament/current/mps.json")
+            .responseJSON { (request, response, JSON, error) in
+                println(JSON)
+                let json = JSON as NSArray
+                for item in json {
+                    let mp = item as NSDictionary
+                    let firstName = mp["first_name"] as String
+                    let lastName = mp["last_name"] as String
+                    let fullName = "\(firstName) \(lastName)"
+                    self.insertNewObject(mp)
+                }
+        }
+    }
 
     func insertNewObject(sender: AnyObject) {
         if objects == nil {
             objects = NSMutableArray()
         }
-        objects.insertObject(NSDate.date(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+        objects.insertObject(sender, atIndex: objects.count)
+        let indexPath = NSIndexPath(forRow: objects.count - 1, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
 
@@ -45,7 +56,7 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             let indexPath = self.tableView.indexPathForSelectedRow()
-            let object = objects[indexPath.row] as NSDate
+            let object = objects[indexPath.row] as NSDictionary
             (segue.destinationViewController as DetailViewController).detailItem = object
         }
     }
@@ -61,16 +72,19 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let object = objects[indexPath.row] as NSDictionary
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
-        let object = objects[indexPath.row] as NSDate
-        cell.textLabel.text = object.description
+        let firstName = object["first_name"] as String
+        let lastName = object["last_name"] as String
+        cell.textLabel.text = "\(firstName) \(lastName)"
         return cell
     }
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
